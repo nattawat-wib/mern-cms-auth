3daeb248312f5883e8b2435f165da6310fd0f1cf
nutella <nattawat.wib@gmail.com>
2022-05-07 16:07:51 +0700

    edit mongoose

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-exports.module = mongoose.connect("mongodb://localhost:27017/mern-cms-auth")
+mongoose.connect("mongodb://localhost:27017/mern-cms-auth")
     .then(() => { console.log("connect DB successfully"); })
     .catch(err => { console.log(`connect DB FAIL: ${err}`); })
