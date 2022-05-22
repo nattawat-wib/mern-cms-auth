fe8bd225bb3cab30ea724c283ae42f4d2f7b14d3
nattawat-wib <nattawat.wib@gmail.com>
2022-05-22 21:14:14 +0700

    edit route protected

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: process.env.NODE_ENV,
+        origin: process.env.CLIENT_URL,
         credentials: true
     })
 );
